b6b56b8f297fe2a8411bb115de639ff33273ae22
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-09-10 21:19:15 -0400

    Created the training data loop method

--- model.py
+++ model.py
@@ -43,3 +43,26 @@ class NeuralNetwork(nn.Module):
         return logits 
 
 model = NeuralNetwork()
+
+#Hyperparameters for the Neural Network
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    #Sets the model to training mode
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        #Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        #Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
