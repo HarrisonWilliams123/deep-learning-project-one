b736a112656cd4f61afbffb9e09be2c8e146cc4a
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-09-10 21:30:50 -0400

    Finished the Basic Neural Network Script

--- model.py
+++ model.py
@@ -85,3 +85,16 @@ def test_loop(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size 
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+#Initialize the loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+
+#Checks the training and test data loops for accuracy and loss
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-----------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
