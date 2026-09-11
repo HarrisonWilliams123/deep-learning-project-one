73d4fc77509655465e164143e5629162a592534f
HarrisonWilliams123 <harrisonwms@gmail.com>
2026-09-10 21:02:57 -0400

    Initializes the data

--- model.py
+++ model.py
@@ -5,3 +5,21 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import v2
 
+#Creates the training and test data split and initializes the dataloaders
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+)
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
